06d44b56e5d0c9ee46634c542f2012b635e54852
luke <lukeabela56@gmail.com>
2024-04-16 00:07:43 +0200

    Removing zero shot stuff

--- archive/zero-shot-object-detection/README.md
+++
@@ -1,9 +0,0 @@
-# Ball Detection
-
-## To run
-
-```bash
-docker build -t zero_shot_detection .
-docker run -it --rm -v ./src:/src zero_shot_detection
-```
-
--- archive/zero-shot-object-detection/src/object_detection/config.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class ObjectDetectionConfig(Enum):
-    CHECKPOINT: str = "google/owlvit-base-patch32"
-    LABELS: list =["person", "table", "net", "ball"]
-    THRESHOLD: float = 0.1
